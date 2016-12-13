d07ef8032c6c248a7c0efd42ed420792ad9c6c66
Franco Prudhomme <francoprud@gmail.com>
2016-12-12 23:49:22 -0300

    Add menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public GameObject optionsPanel;
+    public GameObject instructionsPanel;
+    public GameObject levelSelectPanel;
+    public GameObject levelSelectTitle;
+
+    void Start()
+    {
+        levelSelectTitle.SetActive(false);
+        instructionsPanel.SetActive(false);
+        levelSelectPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void goToLevelSelectPanel()
+    {
+        optionsPanel.SetActive(false);
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void goToInstructionsPanel()
+    {
+        optionsPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+    }
+
+    public void goToOptionsPanel()
+    {
+        instructionsPanel.SetActive(false);
+        levelSelectPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void goToLevel(int level)
+    {
+        SceneManager.LoadScene("Level " + level);
+    }
+}
